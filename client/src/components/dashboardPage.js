12c7fb2ec93e4750ef2e8bca3ce1eeaf61759aed
CrumrineCoder <crumrinecoding@gmail.com>
2019-09-05 10:57:16 -0400

    Placeholders & Reordering
    The form for creating a new song now takes in the link first, then source, then name to lessen confusion.

--- client/src/components/dashboardPage.js
+++ client/src/components/dashboardPage.js
@@ -289,15 +289,15 @@ function DashboardPage(props) {
           <h3 className="dashboardToolHeader">Post a new song</h3>
           <form onSubmit={onHandlePost}>
             <div className="dashboardToolLabel">
-              <label htmlFor="source">Source <input className="dashboardToolInput" autoComplete="off" value={source} onChange={e => setSource(e.target.value)} type="source" name="source" id="source" />
+              <label htmlFor="link">Link <input placeholder="https://www.youtube.com/watch?v=J5FFDj7vH6E" className="dashboardToolInput" autoComplete="off" type="link" name="link" id="link" />
               </label>
             </div>
             <div className="dashboardToolLabel">
-              <label htmlFor="name">Name <input className="dashboardToolInput" autoComplete="off" type="name" name="name" id="name" />
+              <label htmlFor="source">Source <input placeholder="Chrono Trigger" className="dashboardToolInput" autoComplete="off" value={source} onChange={e => setSource(e.target.value)} type="source" name="source" id="source" />
               </label>
             </div>
             <div className="dashboardToolLabel">
-              <label htmlFor="link">Link <input className="dashboardToolInput" autoComplete="off" type="link" name="link" id="link" />
+              <label htmlFor="name">Name <input placeholder="Frog's Theme" className="dashboardToolInput" autoComplete="off" type="name" name="name" id="name" />
               </label>
             </div>
             <div>
